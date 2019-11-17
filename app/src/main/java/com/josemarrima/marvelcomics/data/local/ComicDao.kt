596859334ad5d369f83b18a8625d543760de9faf
JoseMarrima <benderspor@gmail.com>
2019-11-17 10:43:46 +0200

    Updating ComicDao

--- app/src/main/java/com/josemarrima/marvelcomics/data/local/ComicDao.kt
+++ app/src/main/java/com/josemarrima/marvelcomics/data/local/ComicDao.kt
@@ -1,7 +1,17 @@
 package com.josemarrima.marvelcomics.data.local
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface ComicDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertListofComics(listOfComics: List<Comic>)
+
+    @Query("SELECT * from comic")
+    fun getAllComics(): LiveData<List<Comic>>
 }
