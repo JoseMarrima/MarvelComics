c56de9e8344be50513cac05082711b1646f9ea6f
JoseMarrima <benderspor@gmail.com>
2019-11-18 04:13:32 +0200

    Update Resource and ResponseHandler Generic Class, Make app support caching data on room db, Add constant

--- app/src/main/java/com/josemarrima/marvelcomics/data/ComicRepository.kt
+++ app/src/main/java/com/josemarrima/marvelcomics/data/ComicRepository.kt
@@ -1,9 +1,11 @@
 package com.josemarrima.marvelcomics.data
 
 import com.josemarrima.marvelcomics.data.local.ComicDao
+import com.josemarrima.marvelcomics.data.local.asDomainModel
 import com.josemarrima.marvelcomics.data.remote.ComicService
 import com.josemarrima.marvelcomics.model.MarvelResponse
-import com.josemarrima.marvelcomics.util.ResponseHandler
+import com.josemarrima.marvelcomics.util.*
+import retrofit2.HttpException
 import javax.inject.Singleton
 
 @Singleton
@@ -11,7 +13,13 @@ class ComicRepository(val comicDao: ComicDao,
                       val comicService: ComicService,
                       val responseHandler: ResponseHandler) {
 
-    suspend fun getMarvelResponse(): MarvelResponse {
-        val marvelResponse = comicService.getAsyncComicResponse()
+    suspend fun getMarvelResponse(): Resource<MarvelResponse> {
+        return try {
+            val marvelResponse = comicService.getAsyncComicResponse(TS, API_KEY, HASH)
+            comicDao.insertListofComics(marvelResponse.asDomainModel())
+            return responseHandler.handleSuccess(marvelResponse)
+        } catch (e: HttpException) {
+            responseHandler.handleException(e)
+        }
     }
 }
---
+++ app/src/main/java/com/josemarrima/marvelcomics/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.josemarrima.marvelcomics.util
+
+const val DATABASE_NAME = "comics-db"
+
+const val API_KEY = "6d4ccfd248bb773a295a45e01d01152d"
+
+const val TS = "thesoer"
+
+const val HASH = "88df06d58defa99428300337d01722b4"
